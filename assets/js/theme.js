275a72f55fc9e91916507e8badd92c18f4272b44
farazsheraz <farazahmed9094@gmail.com>
2022-05-25 12:53:37 +0500

    header and inner pages

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,10 +1,41 @@
+// NAVBAR START
+function close_offcanvas() {
+    document.querySelector('.mobile-offcanvas.show').classList.remove('show');
+    document.body.classList.remove('offcanvas-active');
+}
+
+function show_offcanvas(offcanvas_id) {
+    document.getElementById(offcanvas_id).classList.add('show');
+    document.body.classList.add('offcanvas-active');
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('[data-trigger]').forEach(function(everyelement) {
+        let offcanvas_id = everyelement.getAttribute('data-trigger');
+        everyelement.addEventListener('click', function(e) {
+            e.preventDefault();
+            show_offcanvas(offcanvas_id);
+        });
+    });
+
+    document.querySelectorAll('.btn-close').forEach(function(everybutton) {
+        everybutton.addEventListener('click', function(e) {
+            e.preventDefault();
+            close_offcanvas();
+        });
+    });
+
+});
+
+// NAVBAR END
+
 $(document).ready(function() {
     // NAV LINK ACTIVE
     jQuery(function($) {
         var path = window.location.href;
         $("nav ul li a").each(function() {
             if (this.href === path) {
-                $(this).addClass("fw-bold");
+                $(this).addClass("color-green-soil");
             }
         });
     });
@@ -89,5 +120,7 @@ $(document).ready(function() {
     });
 
     // SLICK SLIDER END
+
+
 
 });
